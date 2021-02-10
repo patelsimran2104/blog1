d66152e907e894e311b09ad0372b3e275ee5eefa
Simran <patel.simran.2104@gmail.com>
2021-02-10 22:24:31 +0530

    callbacks

--- app/models/info.rb
+++ app/models/info.rb
@@ -1,4 +1,21 @@
 class Info < ApplicationRecord
+
+  after_initialize do |info|
+    puts "You have initialized an object!"
+  end
+
+  after_find do |info|
+    puts "You have found an object!"
+  end
+
+  after_touch do |info|
+    puts "You have touched an object"
+  end
+
+  after_destroy do |info|
+    puts "Article destroyed"
+  end
+
   validates :fullname, presence: true
   validates_each :fullname do |record, attr, value|
     record.errors.add(attr, 'must start with upper case') if value =~ /\A[[:lower:]]/
@@ -11,12 +28,12 @@ class Info < ApplicationRecord
   validates :gender, presence: true
   validates :professionalemail, format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/, message: "Email invalid"},
                                 length: { minimum: 4, maximum: 254 }
-  validates :dob, presence: true
-  
+  validate :validate_dob
 
-  # def dob_date_cannot_be_this
-  #   if dob > Date.today
-  #     errors.add(:dob, "can't be in the future")
-  #   end
-  # end
+  def validate_dob
+    if dob.present? && dob > DateTime.now
+        errors.add(:dob, 'dob cant be in future')
+    end
+  end
+
 end
